116518ea9769cdcd260df99e17eb6c8acb5ba172
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-01-30 18:45:33 +0100

    redux types

---
+++ src/constants/itemConstants.js
@@ -0,0 +1,9 @@
+// constants or Types
+export const GET_ITEMS = 'GET_ITEMS';
+export const ADD_ITEMS = 'ADD_ITEMS';
+export const BANNER_CLOSE = 'BANNER_CLOSE';
+export const DELETE_ITEM = 'DELETE_ITEM';
+export const INCREASE_ITEM = 'INCREASE_ITEM';
+export const DECREASE_ITEM = 'DECREASE_ITEM';
+export const TOTAL_ITEMS = 'GET_ITEMS';
+export const DETAILS = 'DETAILS';
