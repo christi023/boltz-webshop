b22febf7ad3847fafd12369d54fad4d85ce37416
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-02-02 11:18:32 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+// components
+import Header from './Components/Navbar/Header';
+import Home from './Components/Home/Home';
+import About from './Components/About/About';
+import Products from './Components/Products/Products';
+import Details from './Components/Details/Details';
+import Cart from './Components/Cart/Cart';
+import Register from './Components/Register_Login/Register';
+import Login from './Components/Register_Login/Login';
+
+// style
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    <h1>Welcome</h1>
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route path="/products" component={Products} />
+        <Route path="/details/:id" component={Details} />
+        <Route path="/cart" component={Cart} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+      </Switch>
+    </Router>
   );
 }
 
